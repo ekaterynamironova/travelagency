e8fec72e37b276569e2e44134e6e30fb7c97ad5c
Kateryna Mironova <kateryna.mironova@nure.ua>
2019-11-09 02:22:38 +0200

    connection pool added

--- src/main/java/ua/nure/myronova/finalproject/db/DBManager.java
+++ src/main/java/ua/nure/myronova/finalproject/db/DBManager.java
@@ -1,4 +1,51 @@
 package ua.nure.myronova.finalproject.db;
 
+import ua.nure.myronova.finalproject.exception.DBException;
+import ua.nure.myronova.finalproject.exception.Messages;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBManager {
+
+    private static DBManager instance;
+
+    private DataSource ds;
+
+    public static synchronized DBManager getInstance() throws DBException {
+        if (instance == null) {
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    private DBManager() throws DBException {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            ds = (DataSource) envContext.lookup("jdbc/travel");
+        } catch (NamingException ex) {
+            throw new DBException(Messages.ERR_CANNOT_OBTAIN_DATA_SOURCE, ex);
+        }
+    }
+
+    public Connection getConnection() throws DBException {
+        Connection con = null;
+        try {
+            con = ds.getConnection();
+        } catch (SQLException ex) {
+            throw new DBException(Messages.ERR_CANNOT_OBTAIN_CONNECTION, ex);
+        }
+        return con;
+    }
+
+
+
+
+
+
 }
--- src/main/java/ua/nure/myronova/finalproject/exception/Messages.java
+++ src/main/java/ua/nure/myronova/finalproject/exception/Messages.java
@@ -1,4 +1,7 @@
 package ua.nure.myronova.finalproject.exception;
 
 public class Messages {
+
+    public static final String ERR_CANNOT_OBTAIN_DATA_SOURCE = "Cannot obtain the data source";
+    public static final String ERR_CANNOT_OBTAIN_CONNECTION = "Cannot obtain a connection from the pool";
 }
